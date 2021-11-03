aa6db07903e0d82235cd6b6e76c6ba8d4e283e05
Wes Grove <wmgrove@mtu.edu>
2021-11-03 16:32:04 -0400

    Implemented leaf-finding method

--- Main.java
+++ Main.java
@@ -1,3 +1,5 @@
+import java.util.*;
+
 public class Main {
   public static void main(String[] args) {
     //For now, N == 10
@@ -5,6 +7,26 @@ public class Main {
     for (int i = 0; i < input.length; i++) {
       input[i] = (int)(Math.random() * 100);
     }
-    BST<Integer> searchTree = new BST<Integer>(temp);
+    BST<Integer> searchTree = new BST<Integer>(input);
+  }
+
+  private static ArrayList<Integer> getLeaves(BST<Integer> tree) {
+    BST.TreeNode<Integer> root = tree.getRoot();
+    return crawler(root);
+  }
+
+  private static ArrayList<Integer> crawler(BST.TreeNode<Integer> root) {
+    ArrayList<Integer> results = new ArrayList<Integer>();
+    if (root.left == null && root.right == null) {
+      results.add(root.element);
+      return results;
+    }
+    if (root.left != null) {
+      results.addAll(crawler(root.left));
+    }
+    if (root.right != null) {
+      results.addAll(crawler(root.right));
+    }
+    return results;
   }
 }
